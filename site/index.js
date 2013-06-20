6044d46599568356bd363a8117ba781d4a2c892c
Damon Oehlman <damon.oehlman@gmail.com>
2013-06-20 15:00:29 +1000

    Updated to use rtc package and new nicta git endpoints

--- site/index.js
+++ site/index.js
@@ -1,5 +1,17 @@
-var	signaller = require('rtc-signaller'),
-	channel = signaller('test');
+var	signaller = require('rtc/signaller'),
+	channel = signaller({ channel: 'test', debug: true });
 
 // set the transport as the socket.io signaller
-channel.transport = require('rtc-signaller-ws')({ host: 'http://localhost:3000/' });
+channel.setTransport(require('rtc-signaller-ws')({ host: 'http://localhost:3000/' }));
+
+// wait for ready event
+channel.once('ready', function() {
+    console.log('connected to ' + channel.name + ' with ' + channel.peers.length + ' other peers');
+});
+
+channel.on('peer:discover', function(peer) {
+    var connection = channel.connect(peer);
+
+    // add a stream to the connection
+    connection.addStream(media.stream);
+});
