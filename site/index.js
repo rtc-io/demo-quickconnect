8ad2f6cf851b6d253deca39a89ef3aa332bc0e2e
Damon Oehlman <damon.oehlman@gmail.com>
2013-07-29 17:56:43 +1000

    Added sessionid awareness

--- site/index.js
+++ site/index.js
@@ -1,83 +1,75 @@
-var	signaller = require('rtc/signaller').create({
-        channel: 'test',
-        host: location.origin || 'http://rtc.io'
-    }),
-    media = require('rtc/media'),
-    localMedia = media(),
-    crel = require('crel'),
-    commandInput,
-    messageList;
+var	uuid = require('uuid');
+var channelId = window.location.hash || uuid.v4();
+var signaller = require('rtc/signaller').create({
+  channel: channelId,
+  host: 'http://signaller.rtc.io'
+});
+var media = require('rtc/media');
+var localMedia = media();
+var crel = require('crel');
+var commandInput;
+var messageList;
+
+if (! window.location.hash) {
+  window.location.hash = uuid.v4();
+}
 
 // render the local stream
 localMedia.render('.zone.local');
 
 signaller.on('peer:discover', function(id) {
-    var connection = signaller.dial(id),
-        videoElements = [];
+  var connection = signaller.dial(id);
+  var videoElements = [];
 
-    /* some potential nodey apis
 
-    // send the local video to the connection
-    localMedia.pipe(connection.createInput());
+  console.log('discovered peer: ' + id);
 
-    // send connection streams to the media
-    connection.createOutput().pipe(media.inject('.zone.remote'));
-    */
+  // if we already have a stream, then add the stream
+  if (localMedia.stream) {
+    console.log('local video stream active, adding to the connection');
+    connection.addStream(localMedia.stream);
+  }
+  // otherwise, wait for the start and bind
+  else {
+    localMedia.once('start', function(stream) {
+      console.log('local video has started streaming', stream);
+      connection.addStream(stream);
+    });
+  }
 
-    /* some potential more traditional apis
+  connection.on('stream:add', function(stream) {
+    videoElements = videoElements.concat(media(stream).render('.zone.remote'));
+    console.log(videoElements);
+  });
 
-    connection.attach(localMedia);
-    connection.renderTo('.zone.remote');
-    */
-
-    console.log('discovered peer: ' + id);
-
-    // if we already have a stream, then add the stream
-    if (localMedia.stream) {
-        console.log('local video stream active, adding to the connection');
-        connection.addStream(localMedia.stream);
-    }
-    // otherwise, wait for the start and bind
-    else {
-        localMedia.once('start', function(stream) {
-            console.log('local video has started streaming', stream);
-            connection.addStream(stream);
-        });
-    }
-
-    connection.on('stream:add', function(stream) {
-        videoElements = videoElements.concat(media(stream).render('.zone.remote'));
-        console.log(videoElements);
+  // when the connection is closed, remove the streams
+  connection.on('close', function() {
+    // remove the video elements
+    videoElements.splice(0).forEach(function(el) {
+      el.parentNode.removeChild(el);
     });
 
-    // when the connection is closed, remove the streams
-    connection.on('close', function() {
-        // remove the video elements
-        videoElements.splice(0).forEach(function(el) {
-            el.parentNode.removeChild(el);
-        });
-
-        // release the connection reference
-        connection = null;
-    });
+    // release the connection reference
+    connection = null;
+  });
 });
 
 signaller.on('message', function(data) {
-    messageList.appendChild(crel('li', data));
+  messageList.appendChild(crel('li', data));
 });
 
 window.addEventListener('load', function() {
-    messageList = document.getElementById('messageList');
-    commandInput = document.getElementById('commandInput');
-    commandInput.addEventListener('keydown', handleCommand);
+  messageList = document.getElementById('messageList');
+  commandInput = document.getElementById('commandInput');
+  commandInput.addEventListener('keydown', handleCommand);
 });
 
 /* internals */
 
 function handleCommand(evt) {
-    if (evt && evt.keyCode === 13) {
-        signaller.send(commandInput.value);
-        messageList.appendChild(crel('li', { class: 'local' }, commandInput.value));
-        commandInput.select();
-    }
+  if (evt && evt.keyCode === 13) {
+    signaller.send(commandInput.value);
+    messageList.appendChild(crel('li', { class: 'local' }, commandInput.value));
+    commandInput.select();
+  }
 }
