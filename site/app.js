7736f35643527434b8f1325438d8154409b95b11
Damon Oehlman <damon.oehlman@gmail.com>
2013-08-15 09:47:57 +1000

    Changes to logging

--- site/app.js
+++ site/app.js
@@ -1,32 +1,53 @@
 (function() {
   var socket = io.connect('http://localhost:3000');
-  var scope = rtc.signaller(socket, { dataEvent: 'message' });
+  var scope = rtc.signaller(socket, {
+    dataEvent: 'message',
+    openEvent: 'connect'
+  });
+
   var peers = {};
   var localMedia;
   var videoElements = [];
+  var debug = rtc.logger('helloworld');
+
+  rtc.logger.enable('*');
+
+  scope.on('open', function() {
+    debug('scope open');
+  });
 
   function createPeer(data) {
     var connection = rtc.createConnection();
-    console.log('created peer for remote id: ' + data.id);
+    var coupling;
+
+    debug('created peer for remote id: ' + data.id);
 
     // couple to the remote peer using the id over the signalling scope
-    rtc.couple(connection, { id: data.id }, scope);
+    coupling = rtc.couple(connection, { id: data.id }, scope);
+    coupling.once('active', function() {
+      debug('connection active');
+    });
 
     // if we already have a stream, then add the stream
     if (localMedia.stream) {
-      console.log('local video stream active, adding to the connection');
+      debug('local video stream active, adding to the connection');
       connection.addStream(localMedia.stream);
     }
     // otherwise, wait for the start and bind
     else {
       localMedia.once('start', function(stream) {
-        console.log('local video has started streaming', stream);
+        debug('local video has started streaming', stream);
         connection.addStream(stream);
       });
     }
 
     connection.addEventListener('addstream', function(evt) {
+      debug('remote stream added');
       videoElements = videoElements.concat(rtc.media(evt.stream).render('.zone.remote'));
+    });
+
+    connection.addEventListener('removestream', function(evt) {
+      debug('remote stream removed');
     });
 
     return connection;
@@ -42,7 +63,7 @@
   /* peer connection setup */
 
   scope.on('request', function() {
-    console.log(arguments);
+    debug(arguments);
   });
 
   // when another peer is discovered, try and communicate with it
@@ -50,8 +71,18 @@
     if (! peers[data.id]) {
       peers[data.id] = createPeer(data);
 
-      console.log('announcing self');
+      debug('announcing self');
       scope.announce();
+    }
+  });
+
+  scope.on('leave', function(peerId) {
+    if (peers[peerId]) {
+      peers[peerId].close();
+      debug('I lost a good friend today');
+
+      // reset the peer reference
+      peers[peerId] = null;
     }
   });
 
