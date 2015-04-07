7a2470d023c2dd3ba2cd6bf312a74241ba505b07
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-07 16:14:53 +1000

    Created configuration document

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
+// configure the switchboard address
+exports.switchboard = 'https://switchboard.rtc.io';
+
+// initialise constraints that will be used for local media capture
 exports.constraints = { video: true, audio: true };
+
+// initialise common options that are used across rtc.io packages
+exports.options = {
+  plugins: []
+};
